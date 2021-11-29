63664d767cfd90153df96c067be21d215d065b87
Andrew Chapman <andrew.chapman@bc.edu>
2021-11-28 22:33:41 -0500

    Initial Commit

---
+++ BCEventFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BCEventFinder
+//
+//  Created by Andrew Chapman on 11/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
